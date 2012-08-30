b1b907e316acd512bc474a172b1179bf5e779d66
Michael Enger <mien@nodesagency.no>
2012-08-30 10:56:43 +0200

    Add helper function for easing the use of translations

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -8,3 +8,18 @@ if (!Configure::write('Config.language')) {
 	Configure::write('Config.language', 'en');
 }
 App::uses('Translation', 'Translations.Model');
+
+/**
+ * Translation helper function.
+ *
+ * @param string $text   Text to translate
+ * @param array  $params (optional) Parameters to use when replacing translations
+ */
+function t($text, $params = array()) {
+	$text = Translation::translate($text);
+	$text = String::insert($text, $params, array(
+		'before' => '{',
+		'after'  => '}'
+	));
+	return $text;
+}
