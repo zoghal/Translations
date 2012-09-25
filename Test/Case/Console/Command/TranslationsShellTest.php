ae70b4fabf8dbded5358b1f8086005f685d33215
AD7six <andydawson76@gmail.com>
2012-09-25 17:41:39 +0200

    adding cli test stub

---
+++ Test/Case/Console/Command/TranslationsShellTest.php
@@ -0,0 +1,58 @@
+<?php
+App::uses('TranslationsShell', 'Translations.Console/Command');
+App::uses('ConsoleOutput', 'Console');
+App::uses('ConsoleInput', 'Console');
+
+/**
+ * Test case for Translations
+ *
+ **/
+class TranslationsShellTest extends CakeTestCase {
+
+	public $fixtures = array(
+		'plugin.translations.translation',
+	);
+
+/**
+ * Temporary storage for the real root folder
+ *
+ * @var string
+ */
+	protected $_root;
+
+/**
+ * setup test
+ *
+ * @return void
+ */
+	public function setUp() {
+		$out = $this->getMock('ConsoleOutput', array(), array(), '', false);
+		$in = $this->getMock('ConsoleInput', array(), array(), '', false);
+
+		$this->Shell = $this->getMock(
+			'TranslationsShell',
+			array('in', 'out', 'hr', 'err', '_stop'),
+			array($out, $out, $in)
+		);
+
+		parent::setUp();
+	}
+
+/**
+ * Tear down
+ *
+ * @return void
+ **/
+	public function tearDown() {
+		unset($this->Shell);
+		parent::tearDown();
+	}
+
+/**
+ * testImport
+ *
+ * @return void
+ */
+	public function testImport() {
+	}
+}
