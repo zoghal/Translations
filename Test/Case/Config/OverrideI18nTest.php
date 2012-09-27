7075b893795eefb5d39cf5f8544e86340b7b01e4
Christian Winther <cw@nodes.dk>
2012-09-27 17:17:10 +0200

    make the path REAL

--- Test/Case/Config/OverrideI18nTest.php
+++ Test/Case/Config/OverrideI18nTest.php
@@ -16,7 +16,7 @@ class OverrideI18nTest extends CakeTestCase {
  */
 	public function setUp() {
 		$files = get_included_files();
-		$overrides = CakePlugin::path('Translations') . 'Config/override_i18n.php';
+		$overrides = realpath(CakePlugin::path('Translations') . 'Config/override_i18n.php');
 		$this->skipIf(!in_array($overrides, $files), "Cannot test __ overrides if the config file isn't loaded");
 	}
 
