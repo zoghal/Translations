2651da6e888d10044ebef2f056e9ece010cf7893
AD7six <andydawson76@gmail.com>
2012-09-28 15:59:35 +0200

    use a case sensitive collation
    otherwise keys that only differ in case cannot be stored (with the
    unique index)

--- Config/schema.sql
+++ Config/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS `translations` (
   `locale` varchar(5) NOT NULL DEFAULT 'en' COMMENT 'ISO 3166-1 alpha-2 country code + optional (_ + Region subtag). e.g. en_US',
   `domain` varchar(50) DEFAULT 'default',
   `category` varchar(50) DEFAULT 'LC_MESSAGES',
-  `key` varchar(255) NOT NULL,
+  `key` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `value` text,
   PRIMARY KEY (`id`),
   UNIQUE KEY `locale` (`locale`,`domain`,`category`,`key`)
