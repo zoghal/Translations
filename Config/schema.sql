0540e048154f07a6a99479e251e2a8eff0fbed6c
AD7six <andydawson76@gmail.com>
2012-09-28 15:31:19 +0200

    prevent duplicates at the db

--- Config/schema.sql
+++ Config/schema.sql
@@ -1,12 +1,14 @@
 CREATE TABLE IF NOT EXISTS `translations` (
-  `id` char(36) NOT NULL,
+  `id` varchar(36) NOT NULL,
   `application_id` char(36) DEFAULT NULL,
-  `plural_id` char(36) DEFAULT NULL COMMENT 'If this translation has a plural id - this is it',
-  `plural_case` tinyint(2) DEFAULT NULL COMMENT 'Only relevant for plural translations 0-6',
-  `locale` char(5) NOT NULL DEFAULT 'en' COMMENT 'ISO 3166-1 alpha-2 country code + optional (_ + Region subtag). e.g. en_US',
+  `asset_file_id` varchar(36) DEFAULT NULL,
+  `plural_id` varchar(36) DEFAULT NULL COMMENT 'If this translation has a plural id - this is it',
+  `plural_case` int(2) DEFAULT NULL COMMENT 'Only relevant for plural translations 0-6',
+  `locale` varchar(5) NOT NULL DEFAULT 'en' COMMENT 'ISO 3166-1 alpha-2 country code + optional (_ + Region subtag). e.g. en_US',
   `domain` varchar(50) DEFAULT 'default',
   `category` varchar(50) DEFAULT 'LC_MESSAGES',
   `key` varchar(255) NOT NULL,
   `value` text,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `locale` (`locale`,`domain`,`category`,`key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
