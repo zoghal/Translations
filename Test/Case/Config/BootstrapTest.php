55ed0f4455c1679b269a33ab957c442c17676f29
AD7six <andydawson76@gmail.com>
2012-10-05 14:45:40 +0200

    update test case to .. work
    this test requires deconstructing the translation model if it's been
    loaded.

--- Test/Case/Config/BootstrapTest.php
+++ Test/Case/Config/BootstrapTest.php
@@ -18,8 +18,49 @@ class BootstrapTest extends CakeTestCase {
 		$files = get_included_files();
 		$overrides = realpath(CakePlugin::path('Translations') . 'Config/bootstrap.php');
 		$this->skipIf(!in_array($overrides, $files), "Cannot test t function if the bootstrap file isn't loaded");
+
+		parent::setUp();
+
+		// Load config
+		$this->config = array(
+			'Config.defaultLanguage' => Configure::read('Config.defaultLanguage'),
+			'Config.language' => Configure::read('Config.language'),
+			'Cache.disable' => Configure::read('Cache.disable')
+		);
+		Configure::write('Config.defaultLanguage', 'en');
+		Configure::write('Config.language', 'en');
+
+		ClassRegistry::removeObject('Translation');
+		$this->Translation = ClassRegistry::init('Translations.Translation');
+
+		Cache::clear(true, 'default');
+		Translation::reset();
+		Translation::config(array(
+			'useTable' => 'translations',
+			'cacheConfig' => false,
+			'autoPopulate' => false
+		));
 	}
 
+/**
+ * tearDown method
+ *
+ * Reapply original config and destroy traces of the translate model
+ *
+ * @return void
+ */
+	public function tearDown() {
+		foreach ($this->config as $key => $value) {
+			Configure::write($key, $value);
+		}
+
+		ClassRegistry::removeObject('Translation');
+		Translation::reset();
+
+		unset($this->Translation, $this->config);
+
+		parent::tearDown();
+	}
 	public function testForHelperFunction() {
 		$result = t('key_one');
 		$expected = 'Value One';
