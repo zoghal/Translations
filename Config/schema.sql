539799c225b25ed11eb0b567eefc72ff947bd539
AD7six <andydawson76@gmail.com>
2012-10-06 09:12:14 +0200

    need to extend the uniqe index.
    Plural rules will mean we end up storing:
    
        id	key			plural_id	plural_case
    	1   1 Item		null		null
    	2	%d Items	1			0
    	3	%d Items	1			1
    	4	%d Items	1			3
    	5	%d Items	1			4
    	6	%d Items	1			5
    	7	%d Items	1			6
    
    So, ensure we can store that
    
    revised schema.
    
    for singular translations - allow to mark if they have a plural.
    
    This will make it easier to administer plurals in the backend. II
    envision a manage plural form button, which would open a form to allow
    defining the various forms - that'd only be relevant if the plural rules
    are complex enough for "it's the plural form" to be confusing such as:
    
    	Arabic
    	Czech
    	Icelandic
    	Polish
    	Russian
    
    The above list is not exhaustive, these are the languages which IFF we
    use them in the future we need to have the means to handle their plural
    rules and a means to provide a meaningful interface.

--- Config/schema.sql
+++ Config/schema.sql
@@ -1,12 +1,13 @@
 CREATE TABLE IF NOT EXISTS `translations` (
   `id` char(36) NOT NULL,
-  `plural_id` char(36) DEFAULT NULL COMMENT 'If this translation has a plural id - this is it',
-  `plural_case` tinyint(2) DEFAULT NULL COMMENT 'Only relevant for plural translations 0-6',
   `locale` char(5) NOT NULL DEFAULT 'en' COMMENT 'ISO 3166-1 alpha-2 country code + optional (_ + Region subtag). e.g. en_US',
   `domain` varchar(50) DEFAULT 'default',
   `category` varchar(50) DEFAULT 'LC_MESSAGES',
   `key` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `value` text,
+  `has_plural` tinyint(1) DEFAULT 0 COMMENT 'If this is a singlular translation - is there a plural',
+  `singular_key` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT 'If this is a plural translation, this is the singular form',
+  `plural_case` tinyint(2) DEFAULT NULL COMMENT 'Only relevant for plural translations 0-6',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `locale` (`locale`,`domain`,`category`,`key`)
+  UNIQUE KEY `locale` (`locale`,`domain`,`category`,`key`, `plural_case`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
