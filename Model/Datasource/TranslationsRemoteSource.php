67a3f95d8b58f723afc7c5c8fcf854c82d8c58df
AD7six <andydawson76@gmail.com>
2012-10-15 17:04:30 +0200

    extend datasource so we can see the queries in the log

--- Model/Datasource/TranslationsRemoteSource.php
+++ Model/Datasource/TranslationsRemoteSource.php
@@ -1,8 +1,12 @@
 <?php
+App::uses('DboSource', 'Model/Datasource');
+
 /**
  * TranslationsRemoteSource
+ *
+ * Read translations for a remote location, rather than have a local translations table
  */
-class TranslationsRemoteSource extends DataSource {
+class TranslationsRemoteSource extends DboSource {
 
 /**
  * description
@@ -10,6 +14,54 @@ class TranslationsRemoteSource extends DataSource {
  * @var string
  */
 	public $description = 'Remote datasource, for using one application as the source of translations in another';
+
+/**
+ * __construct
+ *
+ * We are not a dbo source - we are secretly a datasource and just want the log functions, hence we extend
+ * DboSource
+ *
+ * @param mixed $config
+ * @param mixed $autoConnect
+ * @return void
+ */
+	public function __construct($config = null, $autoConnect = true) {
+		DataSource::__construct($config);
+		$this->fullDebug = Configure::read('debug') > 1;
+	}
+
+/**
+ * There is only one method which is expected to call this, and it's forLocale
+ *
+ * It is expected to talk to an api which returns the translations for a single domain/language/category
+ *
+ * ### Options
+ *
+ * - log - Whether or not the query should be logged to the memory log.
+ *
+ * @param string $url to request
+ * @param array $options
+ * @param array $params values to be bound to the query
+ * @return mixed Resource or object representing the result set, or false on failure
+ */
+	public function execute($url, $options = array(), $params = array()) {
+		$options += array('log' => $this->fullDebug);
+
+		$t = microtime(true);
+		$curl = new \Nodes\Curl($url);
+		$this->_result = $curl->get()->getResponseBody();
+
+		if ($options['log']) {
+			$this->took = round((microtime(true) - $t) * 1000, 0);
+			$this->numRows = $this->affected = $this->_result['data'] ? count(current($this->_result['data'])) : 0;
+			$this->logQuery($url, $params);
+		}
+
+		if ($this->_result['success']) {
+			return $this->_result['data'];
+		}
+		return false;
+	}
 
 /**
  * read
@@ -25,16 +77,14 @@ class TranslationsRemoteSource extends DataSource {
 		$config = $class::config();
 
 		$url = String::insert($this->config['host'], $queryData['conditions'] + $config);
+		$result = $this->execute($url);
 
-		$curl = new \Nodes\Curl($url);
-		$data = $curl->get()->getResponseBody();
-
-		if (empty($data['success'])) {
-			return array();
+		if (!$result) {
+			return $result;
 		}
 
 		$return = array();
-		foreach (current($data['data']) as $key => $value) {
+		foreach (current($result) as $key => $value) {
 			if (is_array($value)) {
 				foreach ($value as $case => $val) {
 					$return[] = array(
