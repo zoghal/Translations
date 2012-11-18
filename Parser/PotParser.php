171e331cc468913d66b694228c81889819d149d2
AD7six <andydawson76@gmail.com>
2012-11-18 19:32:50 +0100

    po and pot files need to force settings
    the domain for a pot? file is the filename, it's not an option for the
    domain to be overridden.
    
    Pot files should never overwrite existing translations. otherwise, they'll
    wipe them out and replace them with the key value

--- Parser/PotParser.php
+++ Parser/PotParser.php
@@ -2,4 +2,12 @@
 App::uses('PoParser', 'Translations.Parser');
 
 class PotParser extends PoParser {
+
+	public static function parse($file, $defaults = array()) {
+		$return = parent::parse($file, $defaults);
+		$return['settings']['overwrite'] = false;
+
+		return $return;
+	}
+
 }
